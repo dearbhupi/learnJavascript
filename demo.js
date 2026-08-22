ea5feb4b5eaa4d564cec1aa6af9dd89b1ea90e6d
Bhupinder Singh <dearbhupi@yahoo.com>
2026-08-22 10:42:25 -0500

    added basic files

---
+++ demo.js
@@ -0,0 +1,37 @@
+console.log("Hello, World!");
+console.log("This is a demo JavaScript file.");
+console.log(4354+3424);
+
+// primitive data types
+let x= 10; // number
+let y= "Hello"; // string
+let z= true; // boolean
+let a= null; // null
+let b; // undefined
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
+console.log(b);
+
+// object data type
+let person = {
+    name: "John",
+    age: 30,
+    isStudent: false
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.isStudent);
+// the above method of accessing the object properties is called dot notation. we can also access the object properties using bracket notation.
+console.log(person["name"]);
+console.log(person["age"]);
+console.log(person["isStudent"]);
+
+// array data type
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);        
