34f1cc0c9ed698a7c860339c32579d79c16bc93c
Bhupinder Singh <dearbhupi@yahoo.com>
2026-08-31 09:49:28 -0500

    getting started

---
+++ demo1.js
@@ -0,0 +1,5 @@
+console.log("Hello, World!");
+// how to run this code ? we can use the node in termainal or put this file into index file and open in browser
+
+//  ~/VSCodeProjects/chaiPerCode/Chapter1  node demo1.js                                                                                                                                         ✔  09:47:57 AM 
+//    Hello, World!
