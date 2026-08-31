2ff44854927ac2f6ddb1c89ab6a9481e01c0dcb6
Bhupinder Singh <dearbhupi@yahoo.com>
2026-08-31 14:20:18 -0500

    basic variable defined

--- demo1.js
+++ demo1.js
@@ -1,5 +1,50 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
 // how to run this code ? we can use the node in termainal or put this file into index file and open in browser
 
 //  ~/VSCodeProjects/chaiPerCode/Chapter1  node demo1.js                                                                                                                                         ✔  09:47:57 AM 
 //    Hello, World!
+
+const accountId = 12345;
+let accountEmail='dearbhupi@yahoo.com';
+var accountPassword='@12345';
+accountCity='New York';
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+// const value will not chage but let and var can change the value
+// please do not use var as it is old way to declare variable and it can create problem in 
+// future as var can change the value without an scope and it can be accessed outside the block scope. 
+// so please use let and const to declare variable  
+
+
+// console.table({accountId, accountEmail, accountPassword, accountCity}); // this will print the data in table format but it will not print the value of variable as it will print the key and value pair. 
+
+console.table([accountId, accountEmail, accountPassword, accountCity]); // this will print the data in table format and it will print the value of variable as it will print the key and value pair.    
+/*
+ ~/VSCodeProjects/chaiPerCode/Chapter1  main !1  node demo1.js                                                                            ✔  02:10:26 PM 
+
+console.table({accountId, accountEmail, accountPassword, accountCity});
+┌─────────────────┬───────────────────────┐
+│ (index)         │ Values                │
+├─────────────────┼───────────────────────┤
+│ accountId       │ 12345                 │
+│ accountEmail    │ 'dearbhupi@yahoo.com' │
+│ accountPassword │ '@12345'              │
+│ accountCity     │ 'New York'            │
+└─────────────────┴───────────────────────┘
+
+ ~/VSCodeProjects/chaiPerCode/Chapter1  main !1  node demo1.js                                                                            ✔  02:11:06 PM 
+console.table([accountId, accountEmail, accountPassword, accountCity])
+┌─────────┬───────────────────────┐
+│ (index) │ Values                │
+├─────────┼───────────────────────┤
+│ 0       │ 12345                 │
+│ 1       │ 'dearbhupi@yahoo.com' │
+│ 2       │ '@12345'              │
+│ 3       │ 'New York'            │
+└─────────┴───────────────────────┘ 
+*/
+
